311eb45d22f3dd506c546e768eab9b5c406d2d88
Dizzymouse0 <npage1221@gmail.com>
2023-11-13 21:28:57 -0600

    mutation and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,12 +14,12 @@ const typeDefs = `
   type Query {
     users: [User]!
     user(userId: ID!): User
+    me: User
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    removeUser(userId: ID!): User
   }
 `;
 
