d8885a039239bdbb388065048cb994a0f7adc029
Donna Goon <g.donna120@gmail.com>
2023-11-14 16:26:32 -0600

    fixed login form to tailwind

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -3,16 +3,15 @@ import { useState, useEffect } from 'react';
 // import { Form, Button, Alert } from 'tailwindcss';
 import '../styles/index.css';
 
-
 import { useMutation } from '@apollo/client'; 
 import { LOGIN_USER } from '../../utils/mutations';
 
 import Auth from '../../utils/auth';
 
 const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-  const [validated] = useState(false);
+  const [validated, setValidated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
 
   const [login, { error }] = useMutation(LOGIN_USER);
 
@@ -26,7 +25,8 @@ const LoginForm = () => {
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+    setUserFormData({ ...userFormData, 
+      [name]: value });
   };
 
   const handleFormSubmit = async (event) => {
@@ -38,6 +38,7 @@ const LoginForm = () => {
       event.preventDefault();
       event.stopPropagation();
     }
+    setValidated(true);
 
     try {
       const { data } = await login({
@@ -60,43 +61,63 @@ const LoginForm = () => {
 
   return (
     <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
-        </Alert>
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your email'
-            name='email'
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
+    <form className="max-w-md mx-auto mt-8" noValidate validated={validated} onSubmit={handleFormSubmit}>
+      {showAlert && (
+        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4">
+          <span className="inline-block align-middle mr-8">
+            Something went wrong with your login credentials!
+          </span>
+          <button
+            className="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none"
+            onClick={() => setShowAlert(false)}>
+            <span>×</span>
+          </button>
+        </div>
+      )}
 
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.email && userFormData.password)}
-          type='submit'
-          variant='success'>
-          Submit
-        </Button>
-      </Form>
-    </>
+      <div className="mb-4">
+        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+          Email
+        </label>
+        <input
+          type="text"
+          id="email"
+          placeholder="Your email"
+          name="email"
+          onChange={handleInputChange}
+          value={userFormData.email}
+          required
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        <div className="text-red-500 text-xs mt-1">{validated && !userFormData.email && 'Email is required!'}</div>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Your password"
+          name="password"
+          onChange={handleInputChange}
+          value={userFormData.password}
+          required
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        <div className="text-red-500 text-xs mt-1">{validated && !userFormData.password && 'Password is required!'}</div>
+      </div>
+
+      <button
+        disabled={!(userFormData.email && userFormData.password)}
+        type="submit"
+        className="bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Submit
+      </button>
+    </form>
+  </>
   );
 };
 
