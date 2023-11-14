dc4bd64da79f9a84c43f32922fe29ba2a368a0ac
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-11-13 19:13:24 -0600

    modified seeds js

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,31 +1,36 @@
 const db = require('../config/connection');
 const userSeeds = require('./userSeeds.json');
-const commentSeeds = require('./commentSeeds.json');
-const petDB = require('./petDB');
+// const commentSeeds = require('./commentSeeds.json');
 const User = require('../models/user');
 
-db.once('open', async () => {
-  try {
-    // await petDB('Thought', 'thoughts');
-    await petDB(User, 'users');
-    await User.create(userSeeds);
+// const { getRandomName, getRandomAssignments } = require('./data');
 
-    for (let i = 0; i < commentSeeds.length; i++) {
-      const { _id, commentAuthor } = await comment.create(commentSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: commentAuthor },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
-    }
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
 
-  console.log('all done!');
-  process.exit(0);
-});
+await User.collection.insertMany(userSeeds);
+
+
+// db.once('open', async () => {
+//   try {
+//     // await petDB('Thought', 'thoughts');
+//     await petDB(User, 'users');
+//     await User.create(userSeeds);
+
+//     for (let i = 0; i < commentSeeds.length; i++) {
+//       const { _id, commentAuthor } = await comment.create(commentSeeds[i]);
+//       const user = await User.findOneAndUpdate(
+//         { username: commentAuthor },
+//         {
+//           $addToSet: {
+//             thoughts: _id,
+//           },
+//         }
+//       );
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
+
+//   console.log('all done!');
+//   process.exit(0);
+// });
