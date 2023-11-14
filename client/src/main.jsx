2fb70179ce87154fce1301be521e8d219778f6f0
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-11-14 13:05:34 -0600

    modified main jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,27 +9,25 @@ import SignupForm from './components/SignupForm.jsx';
 import Navbar from './components/Navbar.jsx';
 //edit the paths and create the pages to import
 const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <App />,
-      errorElement: <NotFound />,
-
-      children: [
-        {
-          index: true,
-          element: <LoginForm />
-        },
-        {
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <LoginForm />
+      },
+      {
         path: '/signup',
         element: <SignupForm />
-        },
-        {
-          path: '/navbar',
-          element: <Navbar />
-          }, 
-      ],
-    },
-  ]);
+      },
+      {
+        path: '/navbar',
+        element: <Navbar />
+      },
+    ],
+  },
+]);
   
   ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
