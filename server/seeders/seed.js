2a1b4a4739d7c4dbd92998dfd4bc3aa97b171199
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-11-13 19:21:58 -0600

    changes

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,36 +1,25 @@
-const db = require('../config/connection');
+const connection = require('../config/connection');
 const userSeeds = require('./userSeeds.json');
 // const commentSeeds = require('./commentSeeds.json');
 const User = require('../models/user');
 
 // const { getRandomName, getRandomAssignments } = require('./data');
 
+connection.once('open', async () => {
+  try {
 
-await User.collection.insertMany(userSeeds);
+    let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+    if (userCheck.length) {
+      await connection.dropCollection('users');
+    }
 
+    await User.collection.insertMany(userSeeds);
 
-// db.once('open', async () => {
-//   try {
-//     // await petDB('Thought', 'thoughts');
-//     await petDB(User, 'users');
-//     await User.create(userSeeds);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 
-//     for (let i = 0; i < commentSeeds.length; i++) {
-//       const { _id, commentAuthor } = await comment.create(commentSeeds[i]);
-//       const user = await User.findOneAndUpdate(
-//         { username: commentAuthor },
-//         {
-//           $addToSet: {
-//             thoughts: _id,
-//           },
-//         }
-//       );
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     process.exit(1);
-//   }
-
-//   console.log('all done!');
-//   process.exit(0);
-// });
+  console.log('all done!');
+  process.exit(0);
+});
